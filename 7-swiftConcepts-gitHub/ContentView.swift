5dc21dde91a20a7bf642475c4ebecfa2035330d1
Liu Ziyi <ziyi.liu02@gmail.com>
2023-10-09 11:57:45 +0800

    - Add text

--- 7-swiftConcepts-gitHub/ContentView.swift
+++ 7-swiftConcepts-gitHub/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("GitHub Essentials!")
+                .font(.title)
+            
+            Text("Branching")
         }
         .padding()
     }
